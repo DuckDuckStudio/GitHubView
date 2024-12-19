62dbf838b0f676dda3be2018dd63bb132580f2a0
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-12-19 20:13:12 +0800

    Try fix

--- installer/Macos.sh
+++
@@ -1,2 +0,0 @@
-# 创建安装包
-pkgbuild --root ./output --identifier DuckStudio.GitHubView --version 1.0 --install-location /Applications --install-location /Applications --scripts ./scripts GitHubView.pkg
